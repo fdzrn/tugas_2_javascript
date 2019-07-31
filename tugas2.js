40ad21cb615b168abcd5433c67551f2c3d4cbc3d
fdzrn <fadzriangibran@gmail.com>
2019-07-31 14:22:05 +0700

    Tugas 2 Javascript
    Tugas ini saya kumpulkan untuk Niomic.com

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+let lahir = 1996;
+let tempat = 'Sukabumi';
+let lahirNormal = false;
+let punyaKaka = null;
+let anakKe = ["satu",'dari','dua','bersaudara'];
+
+console.log(lahir);
+console.log(tempat);
+console.log(lahirNormal);
+console.log(punyaKaka);
+console.log(anakKe);
